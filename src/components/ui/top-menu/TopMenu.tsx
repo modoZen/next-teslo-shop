db668849e0e6e9794f3a9b1564819b1c0005ebb4
Max Diaz <mdiazn@uni.pe>
2026-03-16 13:42:47 -0500

    TopMenu

---
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -0,0 +1,62 @@
+import { titleFont } from "@/config/fonts";
+import Link from "next/link";
+import { IoCartOutline, IoSearchOutline } from "react-icons/io5";
+
+export const TopMenu = () => {
+  return (
+    <nav className="flex px-5 justify-between items-center w-full">
+      <div>
+        <Link href="/">
+          <span className={`${titleFont.className} antialiased font-bold`}>
+            Teslo
+          </span>
+        </Link>
+      </div>
+
+      <div className="hidden sm:block">
+        <Link
+          className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
+          href="/category/men"
+        >
+          Hombres
+        </Link>
+        <Link
+          className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
+          href="/category/women"
+        >
+          Mujeres
+        </Link>
+        <Link
+          className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
+          href="/category/kids"
+        >
+          Niños
+        </Link>
+      </div>
+
+      <div className="flex items-center">
+        <Link
+          className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
+          href="/search"
+        >
+          <IoSearchOutline className="w-5 h-5" />
+        </Link>
+        <Link
+          className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
+          href="/cart"
+        >
+          <div className="relative">
+            <span className="absolute text-xs rounded-full font-bold bg-blue-500 text-white px-1 -top-2 -right-2">
+              3
+            </span>
+            <IoCartOutline className="w-5 h-5" />
+          </div>
+        </Link>
+
+        <button className="m-2 p-2 rounded-md transition-all hover:bg-gray-100">
+          Menú
+        </button>
+      </div>
+    </nav>
+  );
+};

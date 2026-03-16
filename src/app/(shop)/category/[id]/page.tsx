a424fc5257892154b487e80f267823367021e3d0
Max Diaz <mdiazn@uni.pe>
2026-03-16 15:52:21 -0500

    pagina 404 categorias

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,4 +1,16 @@
-export default function Home() {
+import { notFound } from "next/navigation";
+
+export default async function Home({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  if (id !== "men" && id !== "women" && id !== "kids") {
+    notFound();
+  }
+
   return (
     <div>
       <h1>Category Page</h1>

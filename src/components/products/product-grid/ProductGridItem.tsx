560f5918d8cdb3a2b4e82bfca88008fa21e8ff85
Max Diaz <mdiazn@uni.pe>
2026-03-16 21:23:09 -0500

    cambiar imagen mouseOver

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -1,21 +1,28 @@
+"use client";
+
 import { Product } from "@/interfaces/product.interface";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 interface Props {
   product: Product;
 }
 
 export const ProductGridItem = ({ product }: Props) => {
+  const [display, setDisplay] = useState(product.images[0]);
+
   return (
-    <div className="rounded-md overflow-hidden fade-in">
+    <div className="rounded-md overflowf-hidden fade-in">
       <Link href={`/product/${product.slug}`}>
         <Image
-          src={`/products/${product.images[0]}`}
+          src={`/products/${display}`}
           alt={product.title}
-          className="w-full object-cover"
+          className="w-full object-cover rounded"
           width={500}
           height={500}
+          onMouseEnter={() => setDisplay(product.images[1])}
+          onMouseLeave={() => setDisplay(product.images[0])}
         />
       </Link>
 

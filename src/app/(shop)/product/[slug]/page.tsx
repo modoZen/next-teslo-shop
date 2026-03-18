b8fc4536ee689ccdbe49a4bbbaa9a6dafbfc4bb7
Max Diaz <mdiazn@uni.pe>
2026-03-17 19:25:28 -0500

    pantalla de producto

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,7 +1,33 @@
-export default function Home() {
+import { titleFont } from "@/config/fonts";
+import { initialData } from "@/seed/seed";
+import { notFound } from "next/navigation";
+
+export default async function Home({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+  const product = initialData.products.find((p) => p.slug === slug);
+
+  if (!product) {
+    notFound();
+  }
+
   return (
-    <div>
-      <h1>Order #abc page</h1>
+    <div className="mt-5 mb-20 grid md:grid-cols-3 gap-3">
+      <div className="col-span-1 md:col-span-2 ">hola</div>
+
+      <div className="col-span-1 px-5 ">
+        <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>
+          {product.title}
+        </h1>
+        <p className="text-lg mb-5">${product.price.toFixed(2)}</p>
+
+        <button className="btn-primary my-5">Agregar al carrito</button>
+
+        <h3 className="font-bold text-sm">{product.description}</h3>
+      </div>
     </div>
   );
 }

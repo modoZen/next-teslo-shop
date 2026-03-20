6dca8b3c2fbb0e8db2f63258a8d3f298c5dd0f33
Max Diaz <mdiazn@uni.pe>
2026-03-19 22:38:31 -0500

    Semilla de base de datos - Categorías

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,9 +1,18 @@
 import { prisma } from "../lib/prisma.ts";
+import { initialData } from "./seed.ts";
 
 async function main() {
   await prisma.productImage.deleteMany();
   await prisma.product.deleteMany();
   await prisma.category.deleteMany();
+
+  const { categories, products } = initialData;
+
+  const categoriesData = categories.map((name) => ({ name }));
+
+  await prisma.category.createMany({
+    data: categoriesData,
+  });
 
   console.log("Seed Executed");
 }

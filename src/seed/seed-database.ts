219814c70e2edfa8519409bb0da5e7690c1f147e
Max Diaz <mdiazn@uni.pe>
2026-03-19 22:08:38 -0500

    prisma cliente borrar contenido de tablas

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,7 +1,9 @@
-import { initialData } from "./seed.ts";
+import { prisma } from "../lib/prisma.ts";
 
 async function main() {
-  console.log(initialData);
+  await prisma.productImage.deleteMany();
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
 
   console.log("Seed Executed");
 }

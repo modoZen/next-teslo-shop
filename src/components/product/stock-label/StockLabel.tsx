f63738d51eb2e5c6e0007678207dfd2b0cd37843
Max Diaz <mdiazn@uni.pe>
2026-03-23 19:01:50 -0500

    skeleton - mostrar loading en el stock

--- src/components/product/stock-label/StockLabel.tsx
+++ src/components/product/stock-label/StockLabel.tsx
@@ -10,6 +10,8 @@ interface Props {
 
 export const StockLabel = ({ slug }: Props) => {
   const [stock, setStock] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     getStock();
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -18,11 +20,22 @@ export const StockLabel = ({ slug }: Props) => {
   const getStock = async () => {
     const inStock = await getStockBySlug(slug);
     setStock(inStock);
+    setIsLoading(false);
   };
 
   return (
-    <h1 className={`${titleFont.className} antialiased font-bold text-lg`}>
-      Stock: {stock}
-    </h1>
+    <>
+      {isLoading ? (
+        <h1
+          className={`${titleFont.className} antialiased font-bold text-lg bg-gray-200 animate-pulse`}
+        >
+          &nbsp;
+        </h1>
+      ) : (
+        <h1 className={`${titleFont.className} antialiased font-bold text-lg`}>
+          Stock: {stock}
+        </h1>
+      )}
+    </>
   );
 };

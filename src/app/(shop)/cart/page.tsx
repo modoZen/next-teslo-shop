efa6b7e308d3af6adabd5aa156f5c04ee18e987a
Max Diaz <mdiazn@uni.pe>
2026-03-25 10:52:31 -0500

    mostrar los elementos del carrito de compras

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -1,15 +1,6 @@
-import { QuantitySelector } from "@/components/product/quantity-selector/QuantitySelector";
 import { Title } from "@/components/ui/title/Title";
-import { initialData } from "@/seed/seed";
-import Image from "next/image";
 import Link from "next/link";
-import { redirect } from "next/navigation";
-
-const productsInCart = [
-  initialData.products[0],
-  initialData.products[1],
-  initialData.products[2],
-];
+import { ProductsInCart } from "./ui/ProductsInCart";
 
 export default function Home() {
   // redirect("/empty");
@@ -29,29 +20,7 @@ export default function Home() {
             </Link>
 
             {/* Items */}
-            {productsInCart.map((product) => (
-              <div key={product.slug} className="flex mb-5">
-                <Image
-                  src={`/products/${product.images[0]}`}
-                  width={100}
-                  height={100}
-                  style={{
-                    width: "100px",
-                    height: "100px",
-                  }}
-                  alt={product.title}
-                  className="mr-5 rounded"
-                />
-
-                <div>
-                  <p>{product.title}</p>
-                  <p>${product.price}</p>
-                  <QuantitySelector quantity={3} />
-
-                  <button className="underline mt-3">Remover</button>
-                </div>
-              </div>
-            ))}
+            <ProductsInCart />
           </div>
 
           {/* checkout */}
---
+++ src/app/(shop)/cart/ui/ProductsInCart.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { QuantitySelector } from "@/components/product/quantity-selector/QuantitySelector";
+import { useCartStore } from "@/store/cart/cart-store";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+export const ProductsInCart = () => {
+  const productsInCart = useCartStore((state) => state.cart);
+
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setLoaded(true);
+  }, []);
+
+  if (!loaded) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <>
+      {productsInCart.map((product) => (
+        <div key={`${product.slug}-${product.size}`} className="flex mb-5">
+          <Image
+            src={`/products/${product.image}`}
+            width={100}
+            height={100}
+            style={{
+              width: "100px",
+              height: "100px",
+            }}
+            alt={product.title}
+            className="mr-5 rounded"
+          />
+
+          <div>
+            <Link
+              className="hover:underline cursor-pointer"
+              href={`/product/${product.slug}`}
+            >
+              {product.size} - {product.title}
+            </Link>
+            <p>${product.price}</p>
+            <QuantitySelector
+              quantity={3}
+              onQuantityChanged={(value) => console.log(value)}
+            />
+            <button className="underline mt-3">Remover</button>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};

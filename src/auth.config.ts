9b84c07acb5e7b6acd282b0847d7fb4adc4ebf5a
Max Diaz <mdiazn@uni.pe>
2026-03-25 18:05:59 -0500

    agregar autentificacion NextAuth

---
+++ src/auth.config.ts
@@ -0,0 +1,9 @@
+import type { NextAuthConfig } from "next-auth";
+
+export const authConfig = {
+  pages: {
+    signIn: "/auth/login",
+    newUser: "/auth/new-account",
+  },
+  providers: [],
+} satisfies NextAuthConfig;

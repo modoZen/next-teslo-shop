01683923396cf70e6449bcb0d39f3e0d3c951ec3
Max Diaz <mdiazn@uni.pe>
2026-03-25 19:02:54 -0500

    configuracion de Credentials Provider

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,9 +1,38 @@
 import type { NextAuthConfig } from "next-auth";
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { z } from "zod";
 
 export const authConfig = {
   pages: {
     signIn: "/auth/login",
     newUser: "/auth/new-account",
   },
-  providers: [],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+
+        if (!parsedCredentials.success) {
+          return null;
+        }
+
+        const { email, password } = parsedCredentials.data;
+
+        console.log({ email, password });
+
+        // Buscar el correo
+
+        // Comparar contraseñas
+
+        // Regresar el usuario
+
+        return null;
+      },
+    }),
+  ],
 } satisfies NextAuthConfig;
+
+export const { signIn, signOut, auth } = NextAuth(authConfig);

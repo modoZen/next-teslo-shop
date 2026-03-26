ca6bbf798adbd94507768df75385ed283a5d494d
Max Diaz <mdiazn@uni.pe>
2026-03-25 23:17:44 -0500

    conectar login form con NextAuth

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -21,7 +21,7 @@ export const authConfig = {
 
         const { email, password } = parsedCredentials.data;
 
-        console.log({ email, password });
+        console.log({ lml: { email, password } });
 
         // Buscar el correo
 
